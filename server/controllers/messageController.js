254813e6c637ef83c572e36a157deccb63773169
endemic420 <karanjadennis17@gmail.com>
2025-07-16 23:07:23 +0300

    frontend logic

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,7 +1,11 @@
 const Message = require ("../models/Message");
 
 exports.getRoomMessages = async (req,res)=>{
-    const messages = await Message.find({Room : req.params.roomId}).populate('sender','username').sort({createdAt : 1});
-    res.json(messages)
+    try {
+        const messages = await Message.find({Room : req.params.roomId}).populate('sender','username').sort({createdAt : 1});
+        res.json(messages)
+    } catch (error) {
+        res.status(500).json({error: "Server error"})
+    }
 };
 
